e9458c0785815463cf59cd1de9f39f73334aa402
Pokegear <christoph.ding@gmail.com>
2015-11-11 16:44:42 -0800

    Added test folder and test spec.  Also added testing script and chai to package.JSON.  Starting to build out skeleton for more tests.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 
 var app = express();
 
 // serve static elements
-app.use(express.static(path.resolve(__dirname + '/../test_assets'))) // <------ Insert real asset path
+app.use(express.static(path.resolve(__dirname + '/../test_assets'))) // INSERT REAL ASSET PATH
 
-
-// let's say there is an 
+// Request Handling 
+// 'homepage'
 app.get('/', function(req, res) {
-  res.write(path.resolve(__dirname + '/../test_assets/index.html'));
+  res.status(200).send('you posted GET request to HOMEPAGE');
 });
 
 app.listen(8000);
 console.log("listening in on port 8000")
 module.exports = app;
+
+
