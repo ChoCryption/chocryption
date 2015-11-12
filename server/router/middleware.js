a8088bbb4581136b146622977ac06f539924a18b
nicolasfujita <nickfujita@gmail.com>
2015-11-12 11:14:45 -0800

    Added router and controller to express server

---
+++ server/router/middleware.js
@@ -0,0 +1,23 @@
+var morgan = require('morgan');
+var path = require('path');
+
+
+module.exports = function (app, express) {
+ 
+  var encodeRouter = express.Router();
+  var decodeRouter = express.Router();
+
+  //logging middleware
+  app.use(morgan('dev'));
+
+  //serve status files
+  app.use(express.static(path.resolve(__dirname + '/../client')));
+
+  //route encode requests to the encode router
+  app.use('/api/encode', encodeRouter);
+  //route decode requests to the decode router
+  app.use('/api/decode', decodeRouter);
+
+  require('../encode/encodeRoutes.js')(encodeRouter);
+  require('../decode/decodeRoutes.js')(decodeRouter);
+};
--- server/server.js
+++ server/server.js
@@ -1,38 +1,12 @@
 var express = require('express');
-var path = require('path');
-var fs = require('fs');
-var bodyParser = require('body-parser');
 
 var app = express();
 
-// serve static elements
-app.use(express.static(path.resolve(__dirname + '/../test_assets'))) // INSERT REAL ASSET PATH
-
-// Request Handling 
-// 'homepage'
-app.get('/', function(req, res) {
-  res.status(200).send('you made a GET request to HOMEPAGE'); // INSERT REAL ACTION
-});
-
-app.post('/', function(req, res) {
-  res.status(200).send('you made a POST request to HOMEPAGE'); // INSERT REAL ACTION
-  res.end();
-});
-
-// hypothetical 'other page'
-app.get('/goodbye', function(req, res) {
-  res.status(200).send('you made a GET request to HOMEPAGE'); // INSERT REAL ACTION
-});
-
-app.post('/goodbye', function(req, res) {
-  res.status(200).send('you made a POST request to HOMEPAGE'); // INSERT REAL ACTION
-  res.end();
-});
-
-
+require('./router/middleware.js')(app, express);
 
 app.listen(8000);
 console.log("listening in on port 8000")
+
 module.exports = app;
 
 
