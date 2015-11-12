24319c8fec89d726db1e43424857fabe50ada68f
nicolasfujita <nickfujita@gmail.com>
2015-11-12 12:21:01 -0800

    working on frontend backend api sync

--- server/decode/decodeController.js
+++ server/decode/decodeController.js
@@ -5,24 +5,29 @@ var path = require('path');
 module.exports = {
 
 	decodeMessage: function (req, res, next) {
+    console.log(req);
+    console.log(req.files);
+    console.log(req.files.image);
+    // console.log(req.files.file.path);
 
     // //extract the image from the data, and send it into the service that decodes messages
     // var image = '';
-    // var imageId = uuid.v1();
-    // var imgExt = 
+    // // var imageId = uuid.v1();
+    // // var imgExt = 
 
     // req.on('data', function (data) {
     // 	image += data;
     // });
     // req.on('end', function () {
-    // 	fs.writeFile("../temp/"+imageId+".jpeg", image, 'binary', function(err) {
+    //   console.log(image);
+    // 	fs.writeFile(__dirname+'/tmp', image, function(err) {
     // 		if(err) {
     // 			console.log(err);
     // 		} else {
     // 			console.log("The file was saved!");
     // 		}
-    // 	}
-    // }
+    // 	});
+    // });
   }
   //expect to get a message back, and send it in the res
 
