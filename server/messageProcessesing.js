118b0a9aa3d8b3f80b208c2e025ea3b62fea947e
Aaron Kandle <akandle@gmail.com>
2015-11-12 15:38:23 -0800

    MessageProcessing methods implemented. Checks filetype, converts to png as necessary. TODO: Delete file after encoding. TODO: Delete file after decoding.

--- server/messageProcessesing.js
+++ server/messageProcessesing.js
@@ -1,4 +1,8 @@
 var stego = require('./lib/steg.js');
+var path = require('path');
+var readChunk = require('read-chunk');
+var fileType = require('file-type');
+var lwip = require('lwip');
 
 //Simple functional testing
 stego.encode('cho.png', 'testmessage', 'output7');
@@ -7,4 +11,64 @@ stego.decode('output7.png', function(err, message) {
   console.log(message);
   return message;
 });
+
+var pathToImages = '';
+
+module.exports = {
+  encode: function(imageName, message, callback) {
+    //check image type here
+    var image = [];
+    var fullName = path.join(pathToImages, imageName);
+    var fileBuffer = readChunk.sync(fullName, 0, 262);
+    var imageType = fileType(fileBuffer);
+
+    if (imageType.ext !== 'png') {
+      lwip(fullName, imageType.ext, function(image) {
+        image.writeFile(fullName + '.png', 'png', function(err) {
+          if (err) {
+            console.log(err);
+            callback(err);
+          } else {
+            console.log('Image converted to png.');
+            console.log('Beginning encode process...');
+            stego.encode(fullName + '.png', message, function(err, encodeImageName) {
+              if(err) {
+                console.log(err);
+                callback(err);
+              } else {
+                callback(null, encodeImageName);
+              }
+            });
+          }
+        });
+      });
+    } else if (imageType.ext === 'png') {
+      stego.encode(fullName + '.png', message, function(err, encodeImageName) {
+        if(err) {
+          console.log(err);
+          callback(err);
+        } else {
+          callback(null, encodeImageName);
+        }
+      });
+    } else {
+      console.log('Cannot recognize the filetype: ', imageType.ext);
+      callback(new Error('Filetype is incorrect'));
+    }
+  },
+
+  decode: function(image, callback) {
+    var fullName = path.join(pathToImages, image, '.png');
+    //Do we need to check file type here??
+    stego.decode(fullName, function(err, message) {
+      if (err) {
+        console.log(err);
+        callback(err);
+      } else {
+        console.log('Message has been decoded successfully');
+        callback(null, message);
+      }
+    });
+  }
+};
 
