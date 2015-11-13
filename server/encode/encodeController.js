ca9f9abf70413332bc1aaa5ee81d0027472866c1
nicolasfujita <nickfujita@gmail.com>
2015-11-12 23:44:28 -0800

    Updated CLient to use old posting method for mvp. Need to update this to use ajax instead int he future.
    updated the serverside controllers to handle the encode decode opertaions to and from the encode decode modules.
    
    need to sync up on single naming convention, either encode decode, or encrypt decrypt
    
    refactor message processing code. (currently duplicate code sections, and could make good use of promises)

--- server/encode/encodeController.js
+++ server/encode/encodeController.js
@@ -1,6 +1,6 @@
 var path = require('path')
 var fs = require('fs');
-var stego = require('../lib/steg.js');
+var stego = require('../messageProcessing.js');
 
 module.exports = {
 
@@ -8,11 +8,25 @@ module.exports = {
 
     //expect to get a picture back, and send it in the res
     //encode(img, message, callback)
+    console.log('about to encode the message');
+
+    // console.log(typeof req.body.message);
+    console.log(req.body.message);
+
   	stego.encode('cho', req.body.message, function(err, imageFileName) {
-  		fs.readFile(imageFileName, function(err, data) {
-  			if(err) throw err;
-  			res.send(data);
-  		});
+      console.log('done encoding, about to serve the file');
+      // var file = fs.createReadStream(__dirname + '/../cho.png');
+      // res.pipe(file);
+
+      // var img = fs.readFileSync(__dirname + '/../cho.png');
+
+      console.log('this is the file name i got back from encode: ',imageFileName);
+      res.sendFile(imageFileName);
+
+      // res.on('finish',function() {
+      //   console.log('pipe is done');
+      // });
+
   	});
 
   }
